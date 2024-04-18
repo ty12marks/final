0ade881962eda97f57a8654afd0aef24a09e1182
ty12marks <ty12marks@gmail.com>
2024-04-18 00:01:12 -0400

    New App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          You got this tyler or do you?
-        </a>
-      </header>
-    </div>
-  );
-}
-
-
-import React from 'react';
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './Dashboard';
-import './App.css';  // Assuming you have global styles defined
+import './App.css'; // Only need to import once
 
 function App() {
   return (
