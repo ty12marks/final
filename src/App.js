c8fc0b0350248468a61251fadb38d01001753a35
ty12marks <ty12marks@gmail.com>
2024-04-17 21:48:11 -0400

    New Words

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          You got this tyler
         </a>
       </header>
     </div>
